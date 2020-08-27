ddbd6b287f873978566d320d6013db262eace525
Baby G <geoffrey787@hotmail.com>
2020-08-27 17:03:29 +0100

    Fully functional, if austere, conversion html page. Replicating X delete button problem but does not affect functionality. Ability to compile recipes and measure in all options shown.

--- ImperialToMetric.js
+++ ImperialToMetric.js
@@ -9,8 +9,7 @@ function convert(numeric, measurement, ingredient) {
     } else { 
     switch(measurement) {
         case "cupW":
-            numMeasure = numeric * 125;
-            measurement = "g";
+            measurement = "cupW";
             break;
         case "cupV":
             numMeasure = numeric * 237;
@@ -49,117 +48,175 @@ function convert(numeric, measurement, ingredient) {
             numMeasure = numeric;
             break;
         default:
-        alert("Measurement failed - numeric: " + numeric + " - Measurement: " + measurement); // Probably end the program here if no unit of measurement to convert
+        alert("Measurement failed - numeric: " + numeric + " - Measurement: " + measurement);
         }
     }
     numMeasure = Math.round(numMeasure);
-    if (measurement === "cupW") {
+    if (measurement === "cupW") { 
+        measurement = "g"
         switch(ingredient) {
                 case "flour":               // FLOUR
+                    numMeasure = numeric * 135;
                     break;
                 case "cornflour":
+                    numMeasure = numeric * 120;
                     break;
                 case "breadcrumbs":
+                    numMeasure = numeric * 125;
                     break;
                 case "cheese":              // CHEESE
                 case "grated cheese":
                 case "grated cheddar":
                 case "grated parmesan":
+                    numMeasure = numeric * 125;
                     break;
                 case "cream cheese":
+                    numMeasure = numeric * 225;
                     break;
                 case "sugar":               // SUGARS
                 case "caster sugar":
                 case "granulated sugar":
+                    numMeasure = numeric * 200;
                     break;
                 case "icing sugar":
                 case "confectioner's sugar":
+                case "powdered sugar":
+                case "dusting sugar":
+                    numMeasure = numeric * 120;
                     break;
+                case "light brown sugar":
                 case "brown sugar":
                 case "demerara sugar":
+                    numMeasure = numeric * 200;
                     break;
                 case "muscovado sugar":
                 case "dark brown sugar":
+                    numMeasure = numeric * 200;
                     break;
                 case "butter":              // FATS
                 case "margarine":
+                    numMeasure = numeric * 225;
                     break;
                 case "honey":               // SYRUPS
                 case "molasses":
                 case "treacle":
                 case "syrup":
                 case "golden syrup":
+                    numMeasure = numeric * 340;
                     break;
+                case "uncooked rice":
+                case "raw rice":
                 case "rice":                // RICE & GRAINS
+                    numMeasure = numeric * 200;
                     break;
                 case "cooked rice":
+                    numMeasure = numeric * 195;
                     break;
                 case "semolina":
                 case "couscous":
                 case "ground rice":
+                    numMeasure = numeric * 165;
                     break;
                 case "oats":
                 case "rolled oats":
+                    numMeasure = numeric * 90;
                     break;
                 case "oatmeal":
+                    numMeasure = numeric * 130;
                     break;
                 case "raisins":         // DRIED FRUIT
+                    numMeasure = numeric * 145;
+                    break;
                 case "sultanas":
+                    numMeasure = numeric * 170;
+                    break;
                 case "prunes":
+                    numMeasure = numeric * 175;
                     break;
                 case "currants":
+                    numMeasure = numeric * 144;
                     break;
                 case "dried apricots":
+                    numMeasure = numeric * 190;
                     break;
                 case "glace cherries":
+                    numMeasure = numeric * 210;
                     break;
                 case "prawns":          // MEAT & FISH
+                    numMeasure = numeric * 100;
+                    break;     
                 case "shrimp":
+                    numMeasure = numeric * 100;
                     break;
-                case "beef":
-                case "lamb":
-                case "pork":
-                case "chicken":
-                    break;
-                case "nuts":            // NUTS
+                case "nuts":         // NUTS
+                numMeasure = numeric * 140;
+                    break;           
                 case "chopped nuts":
                 case "ground nuts":
+                    numMeasure = numeric * 100;
+                    break;
+                case "ground almonds":
+                    numMeasure = numeric * 100;
                     break;
                 case "hazelnuts":
+                    numMeasure = numeric * 130;
+                    break;
                 case "almonds":
                 case "whole almonds":
+                    numMeasure = numeric * 150;
                     break;
                 case "walnuts":
                 case "pecans":
+                    numMeasure = numeric * 115;
                     break;
                 case "flaked almonds":
+                case "slivered almonds":
+                    numMeasure = numeric * 85;
                     break;
                 case "lentils":         // PULSES
                 case "split peas":
+                    numMeasure = numeric * 225;
                     break;
                 case "haricot beans":
+                    numMeasure = numeric * 200;
                     break;
                 case "kidney beans":
+                    numMeasure = numeric * 185; // note all pulses dried
                     break;
                 case "onion":           // VEGETABLES
                 case "onions":
+                    numMeasure = numeric * 150;
                     break;
                 case "peas":
-                case "sweetcorn":
+                    numMeasure = numeric * 130;
                     break;
+                case "sweetcorn":
+                    numMeasure = numeric * 175;
+                    break;
+                case "shredded cabbage":
+                case "chopped cabbage":
                 case "cabbage":
+                    numMeasure = numeric * 80;
                     break;
                 case "beansprouts":
+                    numMeasure = numeric * 60;
                     break;
                 case "potatoes":
+                case "raw potato":
+                case "diced potato":
+                    numMeasure = numeric * 150;
                     break;
                 case "mashed potato":
                 case "mashed potatoes":
+                    numMeasure = numeric * 250;
                     break;
                 case "tomatoes":
+                case "tomato":
+                    numMeasure = numeric * 200;
                     break;
-                default: 
-                alert("Failed to recognise ingredient")
+                default:
+                numMeasure = numeric * 150
+                alert("Failed to recognise ingredient, conversion likely inaccurate (used default: 1 cup = 150g)")
         }
         
     }
@@ -172,7 +229,32 @@ function convert(numeric, measurement, ingredient) {
         }
     }
     let finalIngredient = numMeasure + measurement + " " + ingredient;
-    alert(finalIngredient);
-    return finalIngredient;
+    
+    
+    let li = document.createElement("li");              //
+    let inputValue = finalIngredient;                   //
+    let t = document.createTextNode(inputValue);        //      Add ingredient to recipe compiler
+    li.appendChild(t);                                  //
+    document.getElementById("recipe").appendChild(li);  //
 
+
+    let myNodeList = document.getElementsByTagName("LI");       //
+    let i;                                                      //
+    for (i = 0; i < myNodeList.length; i++) {
+        let span = document.createElement("SPAN");              //  Creation of 'x' delete button (practically copy pasted from https://www.w3schools.com/howto/howto_js_todolist.asp)
+        let txt = document.createTextNode("\u00D7");
+        span.className = "close";                               //  MAKES EXTRA 'x' BUTTON AFTER EVERY ENTRY FOR UNKNOWN REASON
+        span.appendChild(txt);                                  //
+        myNodeList[i].appendChild(span);
+    }
+
+    let close = document.getElementsByClassName("close")
+    let x;
+    for (x = 0; x < close.length; x++) {
+        close[x].onclick = function() {                         // Make 'x' delete (practically copy pasted from https://www.w3schools.com/howto/howto_js_todolist.asp)
+            let div = this.parentElement;                       
+            div.style.display = "none";
+        }
+    }
+    //alert(finalIngredient);
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,12 @@
-For use in recipes, converting 'Cup' measurements and Imperial weight and volume to metric.
+For use in recipes, converting 'Cup' measurements and Imperial weight and volume to metric. To use, make sure ImperialToMetric.html & ImperialToMetric.js are both in the same folder and open the .html file in your browser.
 
 Version 1.0:
 - Tablespoons return grams and are likely inaccurate. It would be rare for a recipe to call for dozens of either a liquid or solid in tablespoons. A tablespoon measurement should be easy to access in most situations or judge by eye if unavailable. Revisit tablespoon measurements in later versions.
 - 'Cup (To Weight)' measurement will be inaccurate for most ingredients. Current baseline (125g) is equal to 1 cup plain flour.
 - Currently no option to compile a recipe within the page using multiple ingredient entries. This is the main aim for the v1.1.
+
+Version 1.1:
+- 'Cup (To Weight)' measurement now reasonably accurate by ingredient.
+- Feature to compile a recipe added.
+- Feature to remove individual ingredients after addition to recipe added.
+- Bug of mulitple 'x' delete buttons appearing (1 for every new ingredient submission on each previous ingredient entry). Cause by copy-pasting code from a list-creator project https://www.w3schools.com/howto/howto_js_todolist.asp which, while it has proved to be functional, is not clear to me what section is causing it to create extra 'x' buttons. This is the main issue to be addressed in next version.
